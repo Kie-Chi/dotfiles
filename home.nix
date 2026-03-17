53c2520b3621fde9f793db126ab2c50a245acd11
Kie-Chi <137579437@qq.com>
2026-03-17 15:26:21 +0800

    feat(agent): merge agent options from linux.

--- home.nix
+++ home.nix
@@ -10,6 +10,14 @@
     home.username = secrets.home.user;
     home.homeDirectory = lib.mkForce "${secrets.home.dir}";
     home.stateVersion = "25.11";
+    home.sessionVariables = {
+      API_KEY = secrets.agent.apikey;
+      DASHSCOPE_API_KEY = secrets.agent.apikey;
+      # ANTHROPIC
+      ANTHROPIC_BASE_URL = "https://dashscope.aliyuncs.com/apps/anthropic";
+      ANTHROPIC_API_KEY = secrets.agent.apikey;
+      ANTHROPIC_MODEL = "qwen3.5-plus";
+    };
     programs.home-manager.enable = true;
   };
 }
