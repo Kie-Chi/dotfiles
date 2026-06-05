39e77da35f1ff388896f73c69b34527aae428086
Kie-Chi <137579437@qq.com>
2026-06-05 23:25:29 +0800

    fix(tod): update package hash.

--- modules/devps/pkgs/tod.nix
+++ modules/devps/pkgs/tod.nix
@@ -6,7 +6,7 @@ buildGoModule {
   src = fetchgit {
     url = "https://code.onedev.io/onedev/tod";
     rev = "v2.1.1";
-    hash = "sha256-p/iz/sEwMLRCPtN4jqeGPY7iK6jPTzwV3Pmlh7hQXZw="; 
+    hash = "sha256-TmazWvRU4kLF/5+Vz/kFj9l4djej0HeUZQ5ILgaWCtc=";
   };
   vendorHash = "sha256-CQS24qkCtMZ0RRJ3UAiETZmJcAGpSU57z8m3jvEAUnc=";
   meta = with lib; {
