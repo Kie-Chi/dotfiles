21d6d2e4f11421dbfcfdb0b652b9ab9c985fdeb5
Kie-Chi <137579437@qq.com>
2026-06-06 14:29:55 +0800

    feat(shell): enhance zsh with docker, conda, fzf-tab, zoxide, fzf
    - Add docker to oh-my-zsh plugins
    - Add conda-zsh-completion via fetchFromGitHub
    - Fix fzf-tab file path (share/fzf-tab/fzf-tab.plugin.zsh)
    - Add zsh-completions external plugin
    - Enable zoxide and fzf via programs.zoxide/programs.fzf
    - Switch EDITOR alias from vim to nvim

--- modules/cores/shell.nix
+++ modules/cores/shell.nix
@@ -1,14 +1,15 @@
-{ pkgs, lib, config, secrets, ... }:
+{ pkgs, lib, config, secrets, isDesktop, ... }:
 
 {
   programs.zsh = {
     enable = true;
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
-    
+    historySubstringSearch.enable = true;
+
     oh-my-zsh = {
       enable = true;
-      plugins = [ "git" "sudo" "web-search" "copyfile" "dirhistory" "golang" ];
+      plugins = [ "git" "sudo" "docker" "web-search" "z" "command-not-found" "colored-man-pages" "history" "pip" "python" "golang" ];
     };
 
     plugins = [
@@ -17,18 +18,37 @@
         src = pkgs.zsh-powerlevel10k;
         file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
       }
+      {
+        name = "zsh-completions";
+        src = pkgs.zsh-completions;
+      }
+      {
+        name = "conda-zsh-completion";
+        src = pkgs.fetchFromGitHub {
+          owner = "esc";
+          repo = "conda-zsh-completion";
+          rev = "c3dca5b56cca978d974f821f592ee8f11c32320d";
+          hash = "sha256-4JaoYv1dbUaOs5YxZtlG+Q21CYMk2d0EmPfUdKdoNA4=";
+        };
+        file = "conda-zsh-completion.plugin.zsh";
+      }
+      {
+        name = "fzf-tab";
+        src = pkgs.zsh-fzf-tab;
+        file = "share/fzf-tab/fzf-tab.plugin.zsh";
+      }
     ];
 
     sessionVariables = {
       LANG = "en_US.UTF-8";
-      EDITOR = "vim";
+      EDITOR = if isDesktop then "code" else "nvim";
       DOTFILES_DIR = secrets.dotfiles.path;
     };
 
     shellAliases = {
-      zshconf = "vim ${secrets.dotfiles.path}/modules/core.nix";
-      omzconf = "vim ~/.oh-my-zsh";
-      
+      zshconf = "nvim ${secrets.dotfiles.path}/modules/cores/shell.nix";
+      omzconf = "nvim ~/.oh-my-zsh";
+
       ll = "ls -alh";
       ".." = "cd ..";
       "..." = "cd ../..";
@@ -37,18 +57,28 @@
       py = "python3";
       rcat = "command cat";
       grep = "rg";
-      
+
     };
 
 
     initContent = ''
       [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
-      
+
       ${builtins.readFile ../../files/zsh/opt.zsh}
       ${builtins.readFile ../../files/zsh/func.zsh}
     '';
   };
-  
+
+  programs.zoxide = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
   home.file.".p10k.zsh".source = ../../files/zsh/p10k.zsh;
 
 }
