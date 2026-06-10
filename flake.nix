3b33d334071a74ba923fa9c57df095b472261f4f
Kie-Chi <137579437@qq.com>
2026-06-10 09:43:46 +0800

    feat: migrate to sops-nix + config.nix secret system
    Replace plaintext secrets.nix with sops-nix encrypted YAML +
    config.nix for non-sensitive settings. Add devShell with sops/age
    tools. Keep nixgl/niri-scratchpad-flake inputs (Linux required).

--- flake.nix
+++ flake.nix
@@ -12,9 +12,10 @@
       url = "github:gvolpe/niri-scratchpad";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    sops-nix.url = "github:Mic92/sops-nix";
   };
 
-  outputs = { self, nixpkgs, home-manager, nixgl, niri-scratchpad-flake, ... }:
+  outputs = { self, nixpkgs, home-manager, nixgl, niri-scratchpad-flake, sops-nix, ... }@inputs:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
@@ -26,22 +27,60 @@
         enable32bits = pkgs.stdenv.hostPlatform.isx86;
         enableIntelX86Extensions = pkgs.stdenv.hostPlatform.system == "x86_64-linux";
       }).auto.nixGLDefault;
-      repoSecretsPath = ./secrets.nix;
-      homeSecretsPath = (builtins.getEnv "HOME") + "/.config/dotfiles/secrets.nix";
-      secrets =
-        if builtins.pathExists repoSecretsPath
-        then import repoSecretsPath
-        else if builtins.pathExists homeSecretsPath
-        then import homeSecretsPath
-        else {};
+
+      envHome = builtins.getEnv "HOME";
+      userConfigPath = envHome + "/.config/dotfiles/config.nix";
+      systemConfigPath = "/etc/dotfiles/config.nix";
+
+      cfg =
+        if builtins.pathExists userConfigPath then
+          builtins.trace "DEBUG: Found config at USER path: ${userConfigPath}" (import userConfigPath)
+        else if builtins.pathExists systemConfigPath then
+          builtins.trace "DEBUG: Found config at SYSTEM path: ${systemConfigPath}" (import systemConfigPath)
+        else
+          builtins.trace "DEBUG: No config.nix found! Using empty set." {};
+      debugCfg = builtins.trace "DEBUG: Config Content: ${builtins.toJSON cfg}" cfg;
+      user = cfg.home.user;
+      ageKeyPath = "/home/${user}/.config/sops/age/keys.txt";
     in
     {
       homeConfigurations."default" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
         extraSpecialArgs = {
-          inherit secrets niri-scratchpad-flake nixGLDefault;
+          cfg = debugCfg;
+          niri-scratchpad-flake = niri-scratchpad-flake;
+          nixGLDefault = nixGLDefault;
         };
-        modules = [ ./home.nix ];
+        modules = [
+          ./home.nix
+          sops-nix.homeManagerModules.sops
+          {
+            sops.age.keyFile = ageKeyPath;
+            sops.age.generateKey = false;
+            _module.args.cfg = debugCfg;
+          }
+        ];
+      };
+
+      # --- devShell for setup environment ---
+      devShells.${system}.default = nixpkgs.legacyPackages.${system}.mkShell {
+        packages = with nixpkgs.legacyPackages.${system}; [
+          jq
+          sops
+          age
+          ssh-to-age
+          python3
+          python3Packages.rich
+          python3Packages.prompt-toolkit
+          python3Packages.pyyaml
+          git
+          curl
+          gnupg
+        ];
+        shellHook = ''
+          echo "[DEBUG] devShell: setup environment ready"
+          echo "[DEBUG] Available tools: jq, sops, age, ssh-to-age, python3, rich, prompt_toolkit"
+        '';
       };
     };
 }
